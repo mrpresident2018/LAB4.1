4c40eb8f8848188e5517af5b84df456c7cd7ec21
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2018-03-14 09:22:21 -0400

    asdfasdfasdf

--- LAB4.1/src/Cards.java
+++ LAB4.1/src/Cards.java
@@ -2,5 +2,40 @@
 public class Cards 
 {
 	String [] rank = {"A" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10" , "J" , "Q" , "K"};
-	String [] suit = {};
+	String [] suit = {"Diamond" , "Club" , "Heart" , "Spade"};
+	int [] value = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13};
+	public Cards (String[] rank , String [] suit, int [] value)
+	{
+		this.rank = rank;
+		this.suit = suit;
+		this.value = value;
+	}	
+	public String getRank() 
+	{
+		System.out.println(rank);
+	}
+
+	public String getSuit() 
+	{
+		System.out.println (suit);
+	}
+
+	public int getValue()
+	{
+		System.out.println(value);;
+	}
+	
+	public boolean equals(Card othercard)
+	{
+		if (this.value == othercard.getValue())
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public String toString()
+	{
+		return (this.rank + "of" + this.suit + "has the value" + this.value);
 }
---
+++ LAB4.1/src/Deck.java
@@ -0,0 +1,11 @@
+
+public class Deck 
+{
+	for (int a = 0 ; a < rank.length ; a++)
+	{
+		for (int b = 0 ; b < suit.length ; b++)
+		{
+			unDealt.add(rank[a] ; suit[b] ; value [a]);
+		}
+	}
+}

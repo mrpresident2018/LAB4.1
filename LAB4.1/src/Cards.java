2e5e3275f364561fa8869ac7c13dc0e7ee58ab22
mrpresident2018 <sbansal3625@bths.edu>
2018-03-14 19:24:55 -0400

    Meep

--- LAB4.1/src/Cards.java
+++ LAB4.1/src/Cards.java
@@ -4,6 +4,7 @@ public class Cards
 	String [] rank = {"A" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10" , "J" , "Q" , "K"};
 	String [] suit = {"Diamond" , "Club" , "Heart" , "Spade"};
 	int [] value = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13};
+
 	public Cards (String[] rank , String [] suit, int [] value)
 	{
 		this.rank = rank;
@@ -24,18 +25,17 @@ public class Cards
 	{
 		System.out.println(value);;
 	}
-	
+
 	public boolean equals(Card othercard)
 	{
 		if (this.value == othercard.getValue())
 		{
 			return true;
 		}
-		
 		return false;
 	}
 	
 	public String toString()
 	{
-		return (this.rank + "of" + this.suit + "has the value" + this.value);
+		return ("This is the rank (" + this.rank + ")of " + this.suit + " has the value " + this.value);
 }
--- LAB4.1/src/Deck.java
+++ LAB4.1/src/Deck.java
@@ -1,11 +1,63 @@
-
+import java.util.ArrayList;
 public class Deck 
 {
-	for (int a = 0 ; a < rank.length ; a++)
+	private ArrayList<Card> unDealt = new ArrayList<Card>();
+	private ArrayList<Card> Dealt = new ArrayList<Card>();
+	
+	public Deck (String[] rank, String[] suit, int[] value)
 	{
-		for (int b = 0 ; b < suit.length ; b++)
+		for (int a = 0; a < rank.length; a++)
 		{
-			unDealt.add(rank[a] ; suit[b] ; value [a]);
+			for (int b = 0; b < suit.length; b++)
+			{
+				unDealt.add(new Card(rank[a], suit[b], value[a]));
+			}
+		}
+	}
+	public boolean isEmpty()
+	{
+		if (unDealt.size() == 0)
+		{
+			return true;
+		}
+		return false;
+	}
+	public int size()
+	{
+		return unDealt.size();
+	}
+	public String deal()
+	{
+		Card c = unDealt.remove(0);
+		Dealt.add(c);
+		return c.toString();
+	}
+	public void shuffle()
+	{
+		int posmax = 0;
+		int n = unDealt.size();
+		for (int k = 51; k >= 1; k--)
+		{
+			int r = (int)(Math.random()*50) + 1;
+			unDealt.add(r, unDealt.get(k));
+			unDealt.add(k + 1, unDealt.get(r + 1));
+			unDealt.remove(r + 1);
+			unDealt.remove(k + 1);
+		}
+		for (int i = 0; i < n; i++)
+		{	
+			if (unDealt.get(i).getValue() > unDealt.get(posmax).getValue() && unDealt.get(i).getSuit().trim().toLowerCase().equals("spade"));
+			{
+				posmax = i;
+			}
+		}
+		for (int k = 51; k >= 1; k--)
+		{
+			unDealt.add(posmax, unDealt.get(k));
+			unDealt.add(k + 1, unDealt.get(posmax + 1));
+			unDealt.remove(posmax + 1);
+			unDealt.remove(k + 1);
+			posmax = k;
 		}
 	}
 }
